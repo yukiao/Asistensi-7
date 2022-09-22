99bc41dae1340ec92f6a38d20e1280c1b53d0f7f
indahkurniailahi <indahkurniailahi0@gmail.com>
2022-09-22 20:15:39 +0800

    Tugas Pertama

---
+++ H071211001 (INDAH KURNIA ILAHI)/Tugas Materi_01/offices.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE offices(
+-- 	officecode VARCHAR(10) NOT NULL,
+-- 	city VARCHAR(50) NOT NULL,
+-- 	phone VARCHAR(50) NOT NULL,
+-- 	addressline1 VARCHAR(50) NOT NULL,
+-- 	addressline2 VARCHAR(50),
+-- 	state VARCHAR(50),
+-- 	country VARCHAR(50) NOT NULL,
+-- 	PRIMARY KEY(officecode)
+-- )
+
+-- DESCRIBE offices
+-- ALTER TABLE offices
+-- MODIFY phone INT(20)
+-- ALTER TABLE offices
+-- DROP addressline2
